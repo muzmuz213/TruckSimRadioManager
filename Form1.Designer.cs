42abf7624006ce28ebee100e484f1760e42c46b4
BuildTools <unconfigured@null.spigotmc.org>
2023-06-03 21:07:08 +0300

    non debug

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,11 +32,13 @@
             this.Browse_button = new System.Windows.Forms.Button();
             this.FilePath_Textbox = new System.Windows.Forms.TextBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Confirm_button
             // 
-            this.Confirm_button.Enabled = false;
+            this.Confirm_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.Confirm_button.Location = new System.Drawing.Point(655, 12);
             this.Confirm_button.Name = "Confirm_button";
             this.Confirm_button.Size = new System.Drawing.Size(72, 50);
@@ -47,7 +49,8 @@
             // 
             // Browse_button
             // 
-            this.Browse_button.Location = new System.Drawing.Point(577, 12);
+            this.Browse_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Browse_button.Location = new System.Drawing.Point(574, 12);
             this.Browse_button.Name = "Browse_button";
             this.Browse_button.Size = new System.Drawing.Size(72, 50);
             this.Browse_button.TabIndex = 1;
@@ -57,24 +60,59 @@
             // 
             // FilePath_Textbox
             // 
-            this.FilePath_Textbox.Enabled = false;
-            this.FilePath_Textbox.Location = new System.Drawing.Point(29, 28);
+            this.FilePath_Textbox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FilePath_Textbox.Location = new System.Drawing.Point(26, 12);
+            this.FilePath_Textbox.MinimumSize = new System.Drawing.Size(542, 20);
             this.FilePath_Textbox.Name = "FilePath_Textbox";
+            this.FilePath_Textbox.ReadOnly = true;
             this.FilePath_Textbox.Size = new System.Drawing.Size(542, 20);
             this.FilePath_Textbox.TabIndex = 2;
+            this.FilePath_Textbox.Text = "\r\n\r\n";
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.InitialDirectory = "Documents";
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Location = new System.Drawing.Point(26, 38);
+            this.button1.MaximumSize = new System.Drawing.Size(0, 23);
+            this.button1.MinimumSize = new System.Drawing.Size(269, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(269, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add New Radio Station";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2.Location = new System.Drawing.Point(299, 38);
+            this.button2.MaximumSize = new System.Drawing.Size(0, 23);
+            this.button2.MinimumSize = new System.Drawing.Size(269, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(269, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Remove Radio Station";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(747, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.FilePath_Textbox);
             this.Controls.Add(this.Browse_button);
             this.Controls.Add(this.Confirm_button);
+            this.MinimumSize = new System.Drawing.Size(763, 489);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -88,6 +126,8 @@
         private System.Windows.Forms.Button Browse_button;
         private System.Windows.Forms.TextBox FilePath_Textbox;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -23,7 +23,6 @@ namespace TruckSimRadioManager
             if(openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 FilePath_Textbox.Text = openFileDialog1.FileName;
-                Confirm_button.Enabled = true;
             }
         }
         //Confirm button giving a list of radios
@@ -33,22 +32,18 @@ namespace TruckSimRadioManager
             {
                 // Create an instance of StreamReader to read from a file.
                 // The using statement also closes the StreamReader.
-                using (StreamReader sr = new StreamReader(openFileDialog1.FileName)){
+                using (StreamReader sr = new StreamReader(FilePath_Textbox.Text)){
                     List<string[]> parsed_array = new List<string[]>();
                     while (sr.Peek() >= 0){
                         string line = sr.ReadLine();
                         line = line.Trim();
-
-                        if (line.Contains("stream_data[")){
+                        if (line.Contains("stream_data["))
+                        {
                             string[] parsed_file_data = Parse_Radio_Data(line);
-                            foreach (var item in parsed_file_data)      //temp debug command
-                            {
-                                Console.WriteLine(item.ToString());
-                            }
                             parsed_array.Add(parsed_file_data);
                         }
-
                     }
+
                     GenerateTable(parsed_array, this);
                 }
             }
@@ -78,7 +73,14 @@ namespace TruckSimRadioManager
         {
             // Create a TableLayoutPanel
             TableLayoutPanel tableLayout = new TableLayoutPanel();
-            tableLayout.Dock = DockStyle.Fill;
+            tableLayout.Location = new System.Drawing.Point(29, 72);
+            tableLayout.Size = new System.Drawing.Size(698, 316);
+            tableLayout.AutoScroll = true;
+            tableLayout.BackColor = System.Drawing.Color.White;
+            tableLayout.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | 
+                System.Windows.Forms.AnchorStyles.Bottom)| 
+                System.Windows.Forms.AnchorStyles.Left)| 
+                System.Windows.Forms.AnchorStyles.Right)));
             parentControl.Controls.Add(tableLayout);
             // Set column count
             tableLayout.ColumnCount = data[0].Length;
@@ -89,12 +91,15 @@ namespace TruckSimRadioManager
                 for (int j = 0; j < data[i].Length; j++)
                 {
                     tableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100f / data[i].Length));
-                    Label label = new Label();
+                    TextBox label = new TextBox();
+                    label.ReadOnly = true;
                     label.Text = data[i][j];
                     label.Dock = DockStyle.Fill;
                     tableLayout.Controls.Add(label, j, i);
                 }
             }
         }
+
+
     }
 }
